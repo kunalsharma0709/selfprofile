3bee047c501f3ea4eede076a5eedc19bb9692928
kunalsharma0709 <154346810+kunalsharma0709@users.noreply.github.com>
2026-03-04 23:14:51 +0530

    my whole app

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,5 @@
-import { HORIZONTALNAVBAR } from "./Horizontal";
-import { PROFILECARD } from "./Profile";
-export function LANDING(){
-       
-    return(
-        <div>
-            <HORIZONTALNAVBAR></HORIZONTALNAVBAR>
-            <PROFILECARD></PROFILECARD>
-        </div>
-    )
+import { Navigate } from "react-router-dom";
+
+export function LANDING() {
+  return <Navigate to="/Home" replace />;
 }
